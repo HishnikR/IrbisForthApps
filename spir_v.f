54692e1d5721b1b9d82607cc1ff287edecd53ec8
HishnikR <132210560+HishnikR@users.noreply.github.com>
2023-08-02 23:39:30 +0300

    Add files via upload

--- spir_v.f
+++ spir_v.f
@@ -7,4 +7,15 @@ CREATE PROGRAM[] #PROGRAMSIZE CELLS ALLOT
 
 int pc
 int code^
+int cmd
+
+proc step
+  PROGRAM[] pc -th @ to cmd
+  pc 1 + to pc
+
+endproc
+
+proc HL_M4*4 // index1, index2 --
+
+endproc
 
