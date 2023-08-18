bbec11472393d3b402f642693e0408c7f75866e1
HishnikR <132210560+HishnikR@users.noreply.github.com>
2023-08-19 00:46:17 +0300

    Add files via upload

--- MDPI_circles.f
+++ MDPI_circles.f
@@ -57,29 +57,58 @@ proc p_all
 endproc
 
 0 chart.show
-200 100 1000 500 0 chart.rect
+200 50 1000 500 0 chart.rect
+
+1 chart.show
+200 550 1000 300 1 chart.rect
 
 int #series
 0 to #series
+
+100 constant #R
+create p[] #R cells allot
+
+int nmax
 
 proc run-r
   22.5 x0 f!
   22.5 y0 f!
   #series SERIES.CLEAR
-  100 0 do
+  3 #series SERIES.LINEWIDTH
+  #R 0 do
     i s>f 0.25 f* 1.0 f+ R f!
     R f@
-    p_all
+    p_all sigma f@ f/
+    fdup p[] i -th f!
     #series series.fxy
+  loop
+
+  0 to nmax
+  #R 1 - 1 do
+    p[] i -th f@
+    p[] i 1 - -th f@ f>
+    p[] i -th f@
+    p[] i 1 + -th f@ f> and if
+      nmax 1 + to nmax
+    then
   loop
 endproc
 
 proc run
+
+  99 1 chart.addseries
+  99 series.clear
+  3 99 SERIES.LINEWIDTH
+
   10 0 do
     i 0 chart.addseries
     i to #series
     i s>f 0.5 f* 0.5 f+ sigma f!
     run-r
+
+    sigma f@
+    nmax s>f
+    99 series.fxy
   loop
 endproc
 
