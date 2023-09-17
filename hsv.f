8de0db836dd47b450e0289bf090fc59b828d5654
HishnikR <132210560+HishnikR@users.noreply.github.com>
2023-09-17 03:20:48 +0300

    Add files via upload

--- hsv.f
+++ hsv.f
@@ -6,6 +6,11 @@ int B
 int H
 float S
 float V
+float Vmin
+float Vinc
+float Vdec
+
+int Hi
 
 proc MAX3 // a, b, c -- max
    over over > if drop else swap drop then
@@ -40,6 +45,71 @@ proc RGB->HSV // R, G, B --
   to H
 endproc
 
+proc %byte
+  255.0 f* 100.0 f/ f>s
+endproc
+
+proc HSV->RGB // H, f: S, V --
+  100.0 f* V f!
+  100.0 f* S f!
+  to H
+
+  H 60 / 6 mod to Hi
+  100.0 S f@ f- V f@ f* 100.0 f/ Vmin f!
+
+  // a
+  V f@ Vmin f@ f-
+  H 60 mod s>f f* 60.0 f/
+
+  fdup Vmin f@ f+ Vinc f!
+  V f@ fswap f- Vdec f!
+
+  Hi 0 = if
+    V f@ %byte to R
+    Vinc f@ %byte to G
+    Vmin f@ %byte to B
+  then
+
+  Hi 1 = if
+    Vdec f@ %byte to R
+    V f@ %byte to G
+    Vmin f@ %byte to B
+  then
+
+  Hi 2 = if
+    Vmin f@ %byte to R
+    V f@ %byte to G
+    Vinc f@ %byte to B
+  then
+
+  Hi 3 = if
+    Vmin f@ %byte to R
+    Vdec f@ %byte to G
+    V f@ %byte to B
+  then
+
+  Hi 4 = if
+    Vinc f@ %byte to R
+    Vmin f@ %byte to G
+    V f@ %byte to B
+  then
+
+  Hi 5 = if
+    V f@ %byte to R
+    Vmin f@ %byte to G
+    Vdec f@ %byte to B
+  then
+endproc
+
+
+"red" print
 255 0 0 RGB->HSV H .
+"green" print
 0 255 0 RGB->HSV H .
+"blue" print
 0 0 255 RGB->HSV H .
+
+"hsv->rgb" print
+10 0.7 0.8 HSV->RGB
+R . G . B .
+
