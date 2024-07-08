43514d889a0931cb77e2c2472bf4aae31550618a
HishnikR <132210560+HishnikR@users.noreply.github.com>
2024-07-09 02:18:07 +0300

    Add files via upload

--- wavelet.f
+++ wavelet.f
@@ -10,25 +10,31 @@ int n
 proc calc-wavelet // - wavre, wavim
   signal[]
   wav_re[]
-  n 2 * 1 +
+  n 2 * // 1 +
   [X*Y] // addr1, addr2, n – y –
   signal[]
   wav_im[]
-  n 2 * 1 +
+  n 2 * // 1 +
   [X*Y] // addr1, addr2, n – y –
 endproc
 
-float WAmpl 32767.0 WAmpl f!
+float periods 27.0 periods f!
+
+
+float WAmpl 2.0e10 WAmpl f!
 float wav_k 319.35 wav_k f!
 
-float Ampl 100.0 Ampl f!
+float Ampl 32767.0 Ampl f!
 float fs   1.0e6 fs f!
-float f0   10.0e3 f0 f!
-float phi0 0.10 phi0 f!
+float f0   1.0e3 f0 f!
+float phi0 0.0 phi0 f!
 
-1000 to n
+periods f@ 2.0 f/ fs f@ f* f0 f@ f/ f>s  to n
 
 int ?noise 0 to ?noise
+
+int re
+int im
 
 proc fill-signal
   n 2 * 2 + 0 do
@@ -66,28 +72,68 @@ proc fill_demo
     i wav_im[] i -th @ 2 series.xy
   loop
 
-  "summas:" print
+  "summas: " print
   wav_re[] n 2 * 1 + [SUM] .
   wav_im[] n 2 * 1 + [SUM] .
 
 endproc
 
-0 PANEL.SHOW
-0 30 250 200 0 PANEL.RECT
-0 PANEL.ALIGN.LEFT
-0 SPLITTER.SHOW
-0 SPLITTER.ALIGN.LEFT
 
+proc fill-sinus
+  n 2 * 2 + 0 do
+    i n - 1 - s>f 2.0 f* pi f* f0 f@ f* fs f@ f/ phi0 f@ f- fcos
+    Ampl f@ f* f>s
+    signal[] i -th !
+
+    i n - 1 - s>f 2.0 f* pi f* f0 f@ f* fs f@ f/ fcos
+    WAmpl f@ f* f>s
+    wav_re[] i -th !
+
+    i n - 1 - s>f 2.0 f* pi f* f0 f@ f* fs f@ f/ fsin
+    WAmpl f@ f* f>s
+    wav_im[] i -th !
+  loop
+endproc
+
+proc fourier
+  0 series.clear
+    0 CHART.SHOW
+  0 0 chart.addseries
+  3 0 SERIES.LINEWIDTH
+  0 chart.align.client
+  90 0 do
+     i s>f pi f* 180.0 f/ phi0 f!
+     fill-sinus
+     calc-wavelet
+     s>f s>f
+     i . " " print fpatan phi0 f@ f- fdup f. cr
+     i s>f fswap 0 series.fxy
+  loop
+endproc
+
+
+
+
+
+proc panel0
+
+  0 PANEL.SHOW
+  0 30 250 200 0 PANEL.RECT
+  0 PANEL.ALIGN.LEFT
+  0 SPLITTER.SHOW
+  0 SPLITTER.ALIGN.LEFT
+
+  0 CHART.SHOW
+  0 0 chart.addseries
+  3 0 SERIES.LINEWIDTH
+  1 0 CHART.PARENT.PANEL
+  0 chart.align.client
+endproc
+
+proc panel1
 1 PANEL.SHOW
 1 30 150 500 1 PANEL.RECT
 1 PANEL.ALIGN.TOP
-
-
-0 CHART.SHOW
-0 0 chart.addseries
-3 0 SERIES.LINEWIDTH
-1 0 CHART.PARENT.PANEL
-0 chart.align.client
 
 1 chart.show
 1 chart.align.client
@@ -99,6 +145,8 @@ endproc
 0xFF7F00 2 SERIES.COLOR
 3 2 SERIES.LINEWIDTH
 
+endproc
+
 0 LABEL.SHOW
 10 10 130 25 0 LABEL.RECT
 " Settings" 0 LABEL.TEXT
@@ -108,9 +156,6 @@ endproc
 10 100 200 50 0 BUTTON.RECT
 0 0 BUTTON.PARENT.PANEL
 
-fill_demo
-calc-wavelet
-. .
 
 : explore_phi
   1 series.clear
@@ -136,6 +181,12 @@ calc-wavelet
     2 series.fxy
   loop
 ;
+
+
+// fill_demo
+// calc-wavelet . .
+
+fourier
 
 
 
