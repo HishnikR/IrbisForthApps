ab3dc1e60ce900b09920dc31a4b50e2c1be74571
HishnikR <132210560+HishnikR@users.noreply.github.com>
2024-08-25 23:38:34 +0300

    Add files via upload

--- interferometer.f
+++ interferometer.f
@@ -1,16 +1,91 @@
+
 int size
 
 create x[]
-"E:\XSoft\VNIIFTRI\Sample2_channel1.txt" 0 file.text.open drop
-0 x[] file.text.fx to size
+  "E:\XSoft\VNIIFTRI\Sample2_channel1.txt" 0 file.text.open drop
+  0 x[] file.text.fx to size
 size cells allot
 
 create y[]
-"E:\XSoft\VNIIFTRI\Sample2_channel2.txt" 0 file.text.open drop
-0 y[] file.text.fx to size
+  "E:\XSoft\VNIIFTRI\Sample2_channel2.txt" 0 file.text.open drop
+  0 y[] file.text.fx to size
 size cells allot
 
 size .
+
+int n
+int signal[]
+
+100000 constant MAXSIZE
+create wav_re[] MAXSIZE CELLS ALLOT
+create wav_im[] MAXSIZE CELLS ALLOT
+
+proc calc-wavelet // - wavre, wavim
+  signal[]
+  wav_re[]
+  n 2 * 1 +
+  [FX*FY] // addr1, addr2, n – y –
+  signal[]
+  wav_im[]
+  n 2 * 1 +
+  [FX*FY] // addr1, addr2, n – y –
+endproc
+
+
+proc calc-im // shift - wavim
+  cells
+  signal[] +
+  wav_im[]
+  n 2 * 1 +
+  [FX*FY] // addr1, addr2, n – y –
+endproc
+
+float periods 27.0 periods f!
+
+
+float WAmpl 1.0e6  WAmpl f!
+float wav_k 319.35 wav_k f!
+
+float Ampl 0x200000 s>f Ampl f!
+float fs   1.0e6 fs f!
+float f0   10.0e3 f0 f!
+float phi0 0.0 phi0 f!
+
+periods f@ 2.0 f/ fs f@ f* f0 f@ f/ f>s  to n
+
+int re
+int im
+
+proc fill_demo
+  n 2 * 2 + 0 do
+    i n - 1 - s>f 2.0 f* pi f* f0 f@ f* fs f@ f/ fcos
+    i n - 1 -  s>f wav_k f@ f/ fdup f* -1.0 f* fexp f*
+    WAmpl f@ f*
+    wav_re[] i -th f!
+
+    i n - 1 - s>f 2.0 f* pi f* f0 f@ f* fs f@ f/ fsin
+    i n - 1 - s>f wav_k f@ f/ fdup f* -1.0 f* fexp f*
+    WAmpl f@ f*
+    wav_im[] i -th f!
+  loop
+
+  "k= " print wav_k f@ f.
+  " summas: " print
+  wav_re[] n 2 * 1 + [FSUM] f.
+  wav_im[] n 2 * 1 + [FSUM] f.
+  cr
+
+endproc
+
+fill_demo
+
+proc fk
+  100 0 do
+    fill_demo
+    wav_k f@ 0.01 f+ wav_k f!
+  loop
+endproc
+
 
 0 chart.show
 0 0 chart.addseries
@@ -28,7 +103,7 @@ y[] 10000  1 series.plotfx
 2 chart.show
 2 2 chart.addseries
 3 2 chart.addseries
-0 400 1200 400 2 chart.rect
+0 400 600 400 2 chart.rect
 x[] y[] 5000 2 series.plotfxy
 
 
@@ -65,7 +140,8 @@ float sumxy
 float sumx2
 
 int rsize
-100000 to rsize
+1000000 to rsize
+int start 0 to start
 
 proc regressk
   0.0 sumx f!
@@ -73,19 +149,48 @@ proc regressk
   0.0 sumxy f!
   0.0 sumx2 f!
   rsize 0 do
-    x[] i -th f@ sumx f@ f+ sumx f!
-    y[] i -th f@ sumy f@ f+ sumy f!
-    x[] i -th f@ y[] i -th f@ f* sumxy f@ f+ sumxy f!
-    x[] i -th f@ fdup f* sumx2 f@ f+ sumx2 f!
+    x[] start i + -th f@ sumx f@ f+ sumx f!
+    y[] start i + -th f@ sumy f@ f+ sumy f!
+    x[] start i + -th f@ y[] start i + -th f@ f* sumxy f@ f+ sumxy f!
+    x[] start i + -th f@ fdup f* sumx2 f@ f+ sumx2 f!
   loop
   sumxy f@ rsize s>f f* sumx f@ sumy f@ f* f-
   sumx2 f@ rsize s>f f* sumx f@ fdup f* f-
   f/
-  cr
-  "k=" print f.
-  cr
 endproc
 
 
-regressk
+3 chart.show
+600 400 600 400 3 chart.rect
+4 3 chart.addseries
+5 3 chart.addseries
+
+proc explorek
+  4 series.clear
+  50 0 do
+    i 1000000 * to start
+    i s>f
+    regressk
+    4 series.fxy
+  loop
+endproc
+
+proc explorephase
+4 series.clear
+5 series.clear
+100 0 do
+  x[] i 800000 * + to signal[]
+  calc-wavelet fswap fpatan
+
+  y[] i 800000 * + to signal[]
+  calc-wavelet fswap fpatan f-
+  i s>f fswap
+  4 series.fxy
+loop
+endproc
+
+explorephase
+
+
+
 
